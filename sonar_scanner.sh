7be78d582fe6f6889a0900aba94ddf50993a1fc5
emorare <89947717+emorare@users.noreply.github.com>
2021-10-08 20:25:22 -0600

    Update sonar_scanner.sh

--- sonar_scanner.sh
+++ sonar_scanner.sh
@@ -6,7 +6,7 @@ echo Sonar Scanner Installed!
 echo Running Sonar Scanner...
 # We add here the command that runs sonar scanner
 $(pwd)/sonar-scanner-4.6.2.2472-linux/bin/sonar-scanner -X \
-  -Dsonar.projectKey=class-example \
+  -Dsonar.projectKey=EXF_Edgar_Mora \
   -Dsonar.sources=. \
   -Dsonar.host.url=https://sq.semanticsecurity.org \
   -Dsonar.login=$SONARQUBE_LOGIN
